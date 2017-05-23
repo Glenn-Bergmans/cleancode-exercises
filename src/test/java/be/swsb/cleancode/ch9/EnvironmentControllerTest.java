f70be1cb360b50f5ec4b04e0cec6f24b1419410e
Glenn Bergmans <glenn.bergmans@cegeka.com>
2017-05-23 10:59:34 +0200

    Improved readability

--- src/test/java/be/swsb/cleancode/ch9/EnvironmentControllerTest.java
+++ src/test/java/be/swsb/cleancode/ch9/EnvironmentControllerTest.java
@@ -16,68 +16,71 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class EnvironmentControllerTest {
 
     private EnvironmentController controller;
-    private MockControlHardware hw;
+    private MockControlHardware controlHardware;
 
     @Before
     public void setUp() throws Exception {
-        hw = new MockControlHardware();
-        controller = new EnvironmentController(hw);
+        controlHardware = new MockControlHardware();
+        controller = new EnvironmentController(controlHardware);
     }
 
     @Test
     public void tic_GivenTempTooHot_ThenTurnOnCoolerAndBlower() throws Exception {
         // GIVEN
-        hw.setTemp(TOO_HOT);
+        controlHardware.setTemp(TOO_HOT);
 
         // WHEN
         controller.tic();
 
         // THEN
-        assertThatComponentsAreOn(asList(BLOWER, COOLER));
+        assertThatOnlyTheseComponentsAreOn(BLOWER, COOLER);
     }
 
     @Test
     public void tic_GivenTempTooCold_ThenTurnOnHeaterAndBlower() throws Exception {
         // GIVEN
-        hw.setTemp(TOO_COLD);
+        controlHardware.setTemp(TOO_COLD);
 
         // WHEN
         controller.tic();
 
         // THEN
-        assertThatComponentsAreOn(asList(HEATER, BLOWER));
+        assertThatOnlyTheseComponentsAreOn(HEATER, BLOWER);
     }
 
     @Test
     public void tic_GivenTempWayTooHot_ThenTurnOnHiTempAlarm() throws Exception {
         // GIVEN
-        hw.setTemp(WAY_TOO_HOT);
+        controlHardware.setTemp(WAY_TOO_HOT);
 
         // WHEN
         controller.tic();
 
         // THEN
-        assertThatComponentsAreOn(asList(BLOWER, COOLER, HI_TEMP_ALARM));
+        assertThatOnlyTheseComponentsAreOn(BLOWER, COOLER, HI_TEMP_ALARM);
     }
 
     @Test
     public void tic_GivenTempWayTooCold_ThenTurnOnLoTempAlarm() throws Exception {
         // GIVEN
-        hw.setTemp(WAY_TOO_COLD);
+        controlHardware.setTemp(WAY_TOO_COLD);
 
         // WHEN
         controller.tic();
 
         // THEN
-        assertThatComponentsAreOn(asList(HEATER, BLOWER, LO_TEMP_ALARM));
+        assertThatOnlyTheseComponentsAreOn(HEATER, BLOWER, LO_TEMP_ALARM);
     }
 
-    private void assertThatComponentsAreOn(List<HardwareComponents> hardwareComponents) {
-        hardwareComponents
-                .forEach(hardwareComponent -> assertThat(hw.getStateOf(hardwareComponent)).isEqualTo(ON));
+    private void assertThatOnlyTheseComponentsAreOn(HardwareComponents... hardwareComponents) {
+        List<HardwareComponents> componentsThatShouldBeOn = asList(hardwareComponents);
+
+        componentsThatShouldBeOn
+                .forEach(hardwareComponent -> assertThat(controlHardware.getStateOf(hardwareComponent)).isEqualTo(ON));
+
         stream(HardwareComponents.values())
-                .filter(hardwareComponent -> ! hardwareComponents.contains(hardwareComponent))
-                .forEach(hardwareComponent -> assertThat(hw.getStateOf(hardwareComponent)).isEqualTo(OFF));
+                .filter(hardwareComponent -> ! componentsThatShouldBeOn.contains(hardwareComponent))
+                .forEach(hardwareComponent -> assertThat(controlHardware.getStateOf(hardwareComponent)).isEqualTo(OFF));
     }
 
 }
